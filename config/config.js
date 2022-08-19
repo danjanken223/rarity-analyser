11a135e3e73284830797be7e2b42e1d56462e100
RandyB127 <107383259+RandyB127@users.noreply.github.com>
2022-08-19 13:24:43 +0530

    ee

--- config/config.js
+++ config/config.js
@@ -1,16 +1,16 @@
 const config = {
-    app_name: 'Fire Flyz',
-    app_description: 'Welcome to the World of Fire Flyz. A unique original NFT collection of hand drawn images and backgrounds depicting the Fire Flyz in their different environments.',
+    app_name: 'WeStreamer',
+    app_description: 'The WeStreamers are a collection of 10,420 unique avatars that represent the community of streamers around the world and grants each owner exclusive perks on the WeStream App.',
     collection_file_name: 'collection.json',
     collection_contract_address: '0xaf2BdFe4076DFB3fBA171037fA0083460bE4b126',
-    collection_name: 'Fire Flyz',
-    collection_description: 'Welcome to the World of Fire Flyz. A unique original NFT collection of hand drawn images and backgrounds depicting the Fire Flyz in their different environments.',
+    collection_name: 'WeStreamer',0
+    collection_description: 'The WeStreamers are a collection of 10,420 unique avatars that represent the community of streamers around the world and grants each owner exclusive perks on the WeStream App.',
     collection_id_from: 0,
     ignore_traits: ['date'], 
     sqlite_file_name: 'database.sqlite',
     ga: 'G-BW69Z04YTP',
     main_og_image: 'https://gateway.thirdweb.dev/ipfs/QmaeZ1mTfBraS2APXm3iZsgjSt1VQwDJRN3rpScyZt6PBk/0.png',
-    item_path_name: 'FireFlyz',
+    item_path_name: 'WeStreamer',
     page_item_num: 60,
     content_image_is_video: false,
     content_image_frame: 'circle', // circle, rectangle
