045838cf0628a9cf0a321f69f8211b017c8964c0
RandyB127 <107383259+RandyB127@users.noreply.github.com>
2022-08-30 18:28:58 +0530

    V3

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 const config = {
-    app_name: 'DogechainApes',
+    app_name: 'Dogechain Apes',
     app_description: 'Dogechain Apes is a collection of 10,000 unique Dogechain Ape NFTsâ€” unique digital collectibles living on the Dogechain blockchain.',
     collection_file_name: 'collection.json',
     collection_contract_address: '0x0e4fb5b3f960d1e33b7f9e324c1e44d94c887b98',
